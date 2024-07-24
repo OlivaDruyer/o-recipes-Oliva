82d023da9617891c1e02c2336375876ccdcd7680
Sandrine BUSSI <sandrine.bussi@oclock.school>
2024-07-24 11:28:42 +0200

    Config

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    // 'eslint:recommended',
+  'airbnb',
+  'airbnb-typescript',
+  'airbnb/hooks',
+  'plugin:@typescript-eslint/recommended',
+  // 'plugin:react-hooks/recommended',
+  'plugin:react/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.app.json', // LIGNE TRÈS IMPORTANTE !
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-refresh', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
